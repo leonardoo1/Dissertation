845157566ce0532d0285972f213cc6646f8b1f0d
leonardoo1 <leonardlaci17@gmail.com>
2021-09-07 22:57:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,20 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import "./App.css";
-import Coin from "./Coin";
-import About from "./About";
+import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./CommonViews/Navbar";
+import Main from "./CommonViews/main";
+import Footer from "./CommonViews/Footer";
 
 function App() {
-  const [coins, setCoins] = useState([]);
-  const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1000&page=1&sparkline=false"
-      )
-      .then((res) => {
-        setCoins(res.data);
-      })
-      .catch((error) => console.log(error));
-  }, []);
-
-  const handleChange = (e) => {
-    setSearch(e.target.value);
-  };
-
-  const filteredCoins = coins.filter((coin) =>
-    coin.name.toLowerCase().includes(search.toLowerCase())
-  );
-
   return (
-    <div className="coin-app">
-      <div className="Navbar">
-        <a href="" id="news">News</a>
-        <a href="">Buy</a>
-        <a href="./About.js">About us</a>
-        <a>Miner</a>
-        </div>
-      <div className="coin-search">
-        <a className="coin-text" href="/App.js">CryptoShinobi</a>
-        <form>
-          <input
-            type="text"
-            placeholder="Search a currency"
-            className="coin-input"
-            onChange={handleChange}
-          />
-        </form>
-      </div>
-      {filteredCoins.map((coin) => {
-        return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            image={coin.image}
-            symbol={coin.symbol}
-            marketcap={coin.market_cap}
-            price={coin.current_price}
-            priceChange={coin.price_change_percentage_24h}
-            volume={coin.total_volume}
-            mcaprank={coin.market_cap_rank}
-          />
-        );
-      })}
-      </div>
+
+      <div className="container">
+            <BrowserRouter>
+             <Navbar />
+             <Main />
+             <Footer />
+             </BrowserRouter>
+   </div>
   );
 }
 
